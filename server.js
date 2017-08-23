75814062133e9cb7ec1ce25f649d014a217874a2
tanveer111 <31212981+tanveer111@users.noreply.github.com>
2017-08-23 21:11:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-1', function (req, res) {
+  res.send('article 1 requested and it will be served here');
+});
+
+app.get('/article-2', function (req, res) {
+  res.send('article 2 requested and it will be served here');
+});
+
+app.get('/article-3', function (req, res) {
+  res.send('article 3 requested and it will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
